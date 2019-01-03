8238441201689223c0566c8d5fc432cd918c537c
kachulio1 <jngugi88@gmail.com>
2019-01-03 22:45:00 +0300

    test react-live

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import Praise from "react-praise";
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
 
 const myPhrases = {
-  'joseph': phrase => "HI " + phrase,
-  "*": phrase => "You said " + phrase,
-  
+  joseph: phrase => "HI " + phrase,
+  "*": phrase => "You said " + phrase
 };
 
 const MyAppWithVoice = props => (
@@ -15,7 +15,34 @@ export default class extends Component {
   render() {
     return (
       <div>
-        <MyAppWithVoice />
+        <LiveProvider
+          code="const Wrapper = ({ children }) => (
+  <div style={{
+    background: 'papayawhip',
+    width: '100%',
+    padding: '2rem'
+  }}>
+    {children}
+  </div>
+)
+;
+const Title = () => (
+  <h3 style={{ color: 'palevioletred' }}>
+    Hello World!
+  </h3>
+);
+
+render(
+  <Wrapper>
+    <Title />
+  </Wrapper>
+)"
+          noInline
+        >
+          <LiveEditor />
+          <LiveError />
+          <LivePreview />
+        </LiveProvider>
       </div>
     );
   }
